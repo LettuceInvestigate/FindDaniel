764e8274ecbbf6b72378c73ea056f25ad5ea0bcf
Daniel Cruz-Castro <60457928+DanDigits@users.noreply.github.com>
2023-01-31 22:55:28 -0500

    Fixed doLogout()

--- js/code.js
+++ js/code.js
@@ -89,7 +89,7 @@ function doLogout()
 {
 	userId = 0;
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
+	window.location.href = "/../index.html";
 }
 
 function createUser()
