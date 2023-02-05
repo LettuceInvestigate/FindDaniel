3761b29054b046f122fcd93a10296b5838abb115
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2023-02-04 19:26:14 -0500

    cancel that

--- js/code.js
+++ js/code.js
@@ -3,7 +3,7 @@ const extension = 'php';
 
 let userId = 0;
 let globalCounter = 0;
-let contactArray = new Array();
+let thisisanarray = new Array();
 let frontendUsername;
 
 function doLogin()
@@ -224,9 +224,9 @@ function display()
 	for (let i = 0; i < 5; i++)
 	{
 		//let contactJSON = loadContact();
+		//thisisanarray.push(contactJSON);
 		let contactJSON = '{"Image":"\images\person.png", "Name":"James Bond","Email":"jamesbond007@gmail.com","Phone":"678-678-6789","Relation":"Father","Alive":"Alive"}'
 		let contactInfo = JSON.parse(contactJSON)
-		contactList.push(contactJSON);
 		// check we dont repeat 
 		if (true)
 		{
@@ -351,10 +351,11 @@ function loadContact()
 				let temp = JSON.stringify(xhr.responseText);
 				let jsonObject = JSON.parse(temp );
 				console.log(jsonObject);
-				if (jsonObject.id != -1)
+				if (jsonObject.id == -1)
 				{
-					return jsonObject;
+
 				}
+				else return jsonObject;
 				
 			}
 		};
