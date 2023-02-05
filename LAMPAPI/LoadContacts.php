9caff49afd7dd1e200cfde93b707ae32c31903e2
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2023-02-04 19:55:28 -0500

    Update LoadContacts.php

--- LAMPAPI/LoadContacts.php
+++ LAMPAPI/LoadContacts.php
@@ -57,7 +57,6 @@ function sendResultInfoAsJson( $obj )
 function returnWithInfo( $images, $name, $email, $phone, $relation, $alive, $id)
 {
   $retValue = '{"Image":"' . $images . '","Name":"' . $name . '","Email": "' . $email . '","Phone":"' . $phone . '","Relation":"' . $relation . '","Alive":"' . $alive . '","ID":"' . $id . '","error":""}';
-  '{"id":"' . $id . '","Username":"' . $Username . '","Email": "' . $Email . '","error":""}';
   sendResultInfoAsJson( $retValue );
 }
 
